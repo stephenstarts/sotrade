0cfb62676f55da017882caffbab762b4e4f0a01e
Stephen Delong <stephen@sdelong.co>
2021-08-11 23:43:52 -0400

    updated index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -12,7 +12,7 @@
 
 <script lang="ts">
     import Seo from '$lib/components/SEO.svelte'
-    import { ArchiveIcon } from 'svelte-feather-icons'
+    import { ZapIcon } from 'svelte-feather-icons'
     export let user
 </script>
 
@@ -22,11 +22,11 @@
 <div class="w-full text-center mb-4 flex flex-col place-items-center">
     <div class="banner flex flex-col place-items-center mt-32 mb-4">
         <div>
-            <ArchiveIcon size="4x" class="w-12 h-12 text-gray-600" />
+            <ZapIcon size="4x" class="w-12 h-12 text-gray-600" />
         </div>
-        <h3 class="text-6xl text-red-500"><strong>Svelte</strong></h3>
-        <h3 class="px-3 py-1 text-2xl bg-gray-400 text-white uppercase">Starter Kit</h3>
-        <small class="uppercase font-semibold text-gray-800 mt-4"> Typescript - TailwindCSS - <small class="bg-green-700 text-white px-2">Supabase</small> Authentication + Profile - Alert, Modal, etc.</small>
+        <h3 class="text-6xl text-red-500"><strong>SoTrade.One</strong></h3>
+        <h3 class="px-3 py-1 text-2xl bg-gray-400 text-white uppercase">Simple trading logs</h3>
+        <small class="uppercase font-semibold text-gray-800 mt-4">Coming soon!</small>
     </div>
     {#if user && !user.guest}
         <a class="bg-gray-500 border border-gray-600 text-white px-4 py-2 rounded shadow" href="/profile" title="View Profile">View Profile</a>
